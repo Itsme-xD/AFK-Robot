18bcddb81d83c7cd2c5da026809e091eb380b941
Itsme-xD <180702081+Itsme-xD@users.noreply.github.com>
2024-12-08 17:53:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("24779895"))
+API_HASH = getenv("24ca02336ac39cb748e2946de19814e3")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-LOG_ID = int(getenv("LOG_ID", ""))
+BOT_TOKEN = getenv("7651069480:AAEJOhBH1RxoCUl-L4HH9NhNdl8INxuKwQY")
+LOG_ID = int(getenv("-4706420307", ""))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
+MONGO_DB_URI = getenv("mongodb+srv://ztx1430:<db_password>@cluster0.pagia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+SUDO_USER = list(map(int, getenv("7757912959", "").split()))
